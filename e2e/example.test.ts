2f6c851a6938b658d1530a062650987d132ec693
Tobias Maier <trm217@protonmail.com>
2022-05-10 14:01:43 +0200

    fix example test

--- e2e/example.test.ts
+++ e2e/example.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
     await page.goto('/');
-    await expect(page.locator("h1").textContent()).toHaveText('Playwright');
-  });
+    await expect(page.locator("h1")).toHaveText('next-themes Example');
+});
